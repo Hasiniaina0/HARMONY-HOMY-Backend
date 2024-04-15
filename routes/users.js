5dad30c3622a87763e831d2ab5d519f938330ecb
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-04-15 16:48:56 +0200

    update backend route users

--- routes/users.js
+++ routes/users.js
@@ -44,6 +44,7 @@ router.post("/signup", (req, res) => {
   });
 });
 
+
 router.post("/signin", (req, res) => {
   if (!checkBody(req.body, ["email", "password"])) {
     res.json({ result: false, error: "Missing or empty fields" });
