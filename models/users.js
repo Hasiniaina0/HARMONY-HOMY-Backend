4f9d5cffb69203828569f0be217390603e2b3bc1
Al-Khansae <160146958+Al-Khansae@users.noreply.github.com>
2024-04-15 11:28:02 +0200

    Création routes signin signup

--- models/users.js
+++ models/users.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const { token } = require("morgan");
 
 const userSchema = mongoose.Schema({
- name: String,
- firstname: String,
- dateNaissance: Date,
- email: String,
- phoneNumber: Number,
- password: String,
- aPropos: String,
- description:String,
- type: String,
- photo: String,
+  name: String,
+  firstname: String,
+  dateNaissance: Date,
+  email: String,
+  phoneNumber: Number,
+  token: String,
+  password: String,
+  aPropos: String,
+  description: String,
+  type: String,
+  photo: String,
 });
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
