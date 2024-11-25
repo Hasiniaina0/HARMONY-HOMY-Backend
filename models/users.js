8959e48ed7c730c1670f49c5f30adc592f4cc90d
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-11-25 11:20:30 +0100

    modif users

--- models/users.js
+++ models/users.js
@@ -30,7 +30,7 @@ const userSchema = mongoose.Schema({
   photoProfil: [String],
   photos: [String],
   city: String,
-  available:String,
+  available: String,
 });
 
 const User = mongoose.model("users", userSchema);
